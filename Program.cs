5db0b735469b2f6dc4ebe3a0d4d03569d2728720
Zhivolun <foxa_a@mail.ru>
2023-11-29 11:11:29 +0700

    Added task

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintArray(string[] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        Console.Write("\"");
+        Console.Write(inputArray[i]);
+        Console.Write("\"");
+
+        if (i < inputArray.GetLength(0) - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine();
+}
+
+string[] arr = {"go", "09876", ":o)", "45"};
+int size = arr.Length;
+int i = 0;
+int j = 0;
+string[] res = {};
+while (i < size)
+{
+    if (arr[i].Length <= 3)
+    {
+        j++;
+        Array.Resize (ref res, j);
+        res [j - 1] = arr[i];
+    }
+    i++;
+}
+PrintArray(res);
